b614df41fdb5a233724e9a66bdc9eaaa04b9fc00
name <arshadalib007@gmail.com>
2026-03-15 23:51:07 +0530

    added the config file

---
+++ apps/api/src/shared/config/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv"
+
+dotenv.config
+
+const config = {
+      
+   jwt: {
+        secret: ( process.env.JWT_SECRET || "SABKA_VALINTINE_WEEK_KAISE_JA_RAHA_HAI") as string ,
+        expiresIn: (process.env.JWT_EXPIRES_IN  || '24h')as string,
+   }
+
+
+}
