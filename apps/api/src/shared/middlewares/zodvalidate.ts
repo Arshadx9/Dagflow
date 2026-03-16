9a66578523b18d864d558e0fe6d73704d4cf7c7a
name <arshadalib007@gmail.com>
2026-03-16 12:13:54 +0530

    added zod validation middleware

---
+++ apps/api/src/shared/middlewares/zodvalidate.ts
@@ -0,0 +1,21 @@
+import { NextFunction } from "express"
+import z from "zod"
+import Apperror from "../utils/Apperror"
+
+const ZodValidate = (schema : z.ZodSchema) =>{
+
+    return (req : Request , res : Response , next : NextFunction)=>{
+        const result = schema.safeParse(req.body)
+
+  if(!result.success){
+        const message = result.error.issues[0]?.message??"Validation error"
+        throw new Apperror(500 ,message)
+  }
+
+next()
+    }
+      
+
+}
+
+export default ZodValidate
