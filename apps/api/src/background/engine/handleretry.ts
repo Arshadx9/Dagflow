989abf2324d49bf30503ebb19a86bacbe65dbd00
Arshad <arshadalib007@gmail.com>
2026-08-05 22:14:43 +0530

    add retry mechanism

--- apps/api/src/background/engine/handleretry.ts
+++ apps/api/src/background/engine/handleretry.ts
@@ -2,6 +2,7 @@ import { Status } from "../../generated/prisma/client"
 import { defaultjobs } from "../queue/queueconfig"
 import { pushtoqueue } from "../queue/producer"
 import { Jobrunendtime, JobRunStatus, UpdateStepRunStatus } from "./statemanager"
+import prisma from "../../shared/utils/Prisma"
 
 type jobdata = {
 	stepRunId: string
@@ -18,6 +19,14 @@ export const handleretry = async (currenRetries: number, jobdata: jobdata) => {
 	let maxRetries = defaultjobs.attempts
 
 	if (currenRetries < maxRetries) {
+		await prisma.stepRun.update({
+			where: { steprunid: jobdata.stepRunId },
+			data: {
+				retries: { increment: 1 },
+				status: Status.RETRYING,
+			},
+		})
+
 		await pushtoqueue(jobdata)
 		return
 	} else {
--- apps/api/src/background/queue/queueconfig.ts
+++ apps/api/src/background/queue/queueconfig.ts
@@ -1,7 +1,7 @@
 export const QUEUE_NAME = "step-execution"
 
 export const defaultjobs = {
-	attempts: 1,
+	attempts: 3,
 	backoff: {
 		type: "exponential" as const,
 		delay: 2000,
